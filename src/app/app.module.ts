28e0fe982a3ff31010df710f6f43b31aad542ff2
Doan The Anh <doantheanh172tb@gmail.com>
2017-11-21 00:42:14 +0700

    ngFor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppComponent } from './app.component';
 import { TitleComponent } from './title/title';
 import { BookComponent } from './book/book.component';
 import { UserFormComponent } from './user-form/user-form.component';
+import { WordsComponent } from './words/words.component';
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import { UserFormComponent } from './user-form/user-form.component';
     TitleComponent,
     BookComponent,
     UserFormComponent,
+    WordsComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/words/words.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-words',
+  templateUrl: './words.component.html',
+  styleUrls: ['./words.component.css']
+})
+export class WordsComponent implements OnInit {
+  newVn = "";
+  newEn = "";
+  arrWords = [
+    { id: 1, en: 'action', vn: 'Hành động', memorized: true },
+    { id: 2, en: 'action 1', vn: 'Hành động 1', memorized: false },
+    { id: 3, en: 'action 2', vn: 'Hành động 2', memorized: true },
+    { id: 4, en: 'action 3', vn: 'Hành động 3', memorized: false },
+    { id: 5, en: 'action 4', vn: 'Hành động 4', memorized: false },
+    { id: 6, en: 'action 5', vn: 'Hành động 5', memorized: true }
+  ]
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  addWord() {
+    this.arrWords.unshift({
+      id: this.arrWords.length + 1,
+      en: this.newEn,
+      vn: this.newVn,
+      memorized: false
+    })
+    this.newEn = "";
+    this.newVn = "";
+  }
+
+}
