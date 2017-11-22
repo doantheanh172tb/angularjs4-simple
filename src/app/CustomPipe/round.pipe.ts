9aa256e50c64e35c00d0602fc51a52fa518353c2
Doan The Anh <doantheanh172tb@gmail.com>
2017-11-22 22:50:51 +0700

    4: custom pipe multi agrs

--- src/app/CustomPipe/round.pipe.ts
+++ src/app/CustomPipe/round.pipe.ts
@@ -5,7 +5,10 @@ import { Pipe, PipeTransform } from '@angular/core'
 })
 
 export class RoundPipe implements PipeTransform {
-    transform(inputNum: number): number {
-        return Math.round(inputNum);
+    transform(inputNum: number, isUpper: boolean, upTo: number): number {
+        if (isUpper) {
+            return Math.ceil(inputNum) + upTo;
+        }
+        return Math.floor(inputNum) + upTo;
     }
 }
