8cf5c11018ed7c8508b24798d2e34c8d8574c864
Doan The Anh <doantheanh172tb@gmail.com>
2017-11-22 22:08:13 +0700

    3: init cardComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import { Parent1Component } from './parent1/parent1.component';
 import { Child1Component } from './child1/child1.component';
 import { Child2Component } from './child2/child2.component';
 import { Parent2Component } from './parent2/parent2.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
@@ -32,6 +33,7 @@ import { Parent2Component } from './parent2/parent2.component';
     Child1Component,
     Child2Component,
     Parent2Component,
+    CardComponent,
   ],
   imports: [
     BrowserModule,
