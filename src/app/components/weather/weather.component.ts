bd96755694e1408931e11f53ce72fd33e246694b
Doan The Anh <doantheanh172tb@gmail.com>
2017-11-24 00:26:38 +0700

    7: weather isLoading vs error

--- src/app/components/weather/weather.component.ts
+++ src/app/components/weather/weather.component.ts
@@ -13,19 +13,34 @@ export class WeatherComponent implements OnInit {
   txtCityName = '';
   cityName = '';
   temp: null;
+  isLoading = false;
   constructor(private weather: WeatherService) { }
   ngOnInit() { }
 
   getWeather() {
+    this.isLoading = true;
     this.weather.getTemp(this.txtCityName)
       .then(res => {
         console.log(res);
         this.cityName = this.txtCityName;
         this.temp = res;
+        this.isLoading = false;
       })
       .catch(err => {
         console.log(err);
+        alert('cannot find you city!')
+        this.isLoading = false;
+        this.cityName = '';
       });
+  }
+
+  getMessage() {
+    if (this.isLoading) {
+      console.log('Loading');
+      return 'Loading..';
+    }
+    console.log('load');
+    return this.cityName === '' ? 'Enter your city' : (this.cityName + ' is now ' + this.temp);
   }
 
 }
