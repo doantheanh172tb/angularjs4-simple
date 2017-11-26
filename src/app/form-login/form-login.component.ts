8e7decbd035057f567ec3f1f7a29444e0a5c37dd
Doan The Anh <doantheanh172tb@gmail.com>
2017-11-26 14:26:10 +0700

    8: submit to server

--- src/app/form-login/form-login.component.ts
+++ src/app/form-login/form-login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import 'rxjs/add/operator/toPromise';
 
 @Component({
   selector: 'app-form-login',
@@ -6,13 +8,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form-login.component.css']
 })
 export class FormLoginComponent implements OnInit {
- 
+
   onSubmit(formSignIn) {
-    console.log(formSignIn);
+    const url = "http://localhost:3000/signin";
+    const headers = new Headers({
+      'Content-Type': 'application/json'
+    })
+    const body = JSON.stringify(formSignIn.value);
+    this.http.post(url, body, { headers })
+      .toPromise()
+      .then(res => res.json())
+      .then(resJson => console.log(resJson))
+      .catch(err => console.log(err));
   }
 
-  constructor() { }
+  constructor(private http: Http) { }
 
+  postToExpress() {
+    const url = "http://localhost:3000/signin";
+    const headers = new Headers({
+      'Content-Type': 'application/json'
+    })
+    const body = JSON.stringify({ name: 'the anh' });
+    this.http.post(url, body, { headers })
+      .toPromise()
+      .then(res => res.json())
+      .then(resJson => console.log(resJson))
+      .catch(err => console.log(err));
+  }
   ngOnInit() {
   }
 
